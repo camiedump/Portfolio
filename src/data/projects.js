b5181c7d329fa1e4c8f2165116b860b0d61a73d9
camille labial <camiedumb@gmail.com>
2025-05-04 11:25:20 +0800

    update on toml

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ export const projects = [
       technologies: ["Reactjs", "JavaScript", "PokeAPI"],
       image: "/project-pixelmon.png",
       githubLink: "https://github.com/camiedump/Pixelmon.git",
-      liveLink: "*****",
+      liveLink: "404 Not Found",
       category: "Web Application"
     },
     {
@@ -25,7 +25,7 @@ export const projects = [
       description: "A web-based system designed to efficiently monitor on-the-job training activities using QR code technology for real-time attendance tracking, task validation, and trainee progress management.",
       technologies: ["PHP", "HTMl", "CSS", "JavaScript"],
       image: "/project-ojtmis.png",
-      githubLink: "*",
+      githubLink: "404 Not Found",
       liveLink: "https://mediumturquoise-shrew-168289.hostingersite.com/",
       category: "Web Application"
     },
@@ -35,7 +35,7 @@ export const projects = [
       description: "Where hustle meets heart. This is more than a game — it’s a way of life. Dedication, discipline, and drive lead the way. Play with pride, finish with no regrets.",
       technologies: ["Photoshop", "Canva"],
       image: "/project-palaro.jpg",
-      githubLink: "*",
+      githubLink: "404 Not Found",
       liveLink: "https://www.canva.com/design/DAGmYTLRvj0/2PX4A7M0rwANGAS2tjO_dQ/edit?utm_content=DAGmYTLRvj0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton ",
       category: "Graphic Design"
     },
@@ -45,7 +45,7 @@ export const projects = [
       description:"It’s time to unwind and turn up the fun. The beach is our dance floor, the stars our ceiling, and the ocean our backdrop. Come for the sunshine, stay for the sunset party vibes. Let’s make this summer one to remember!",
       technologies: ["Canva"],
       image: "/project-beachparty.png",
-      githubLink: "*",
+      githubLink: "404 Not Found",
       liveLink: "https://www.canva.com/design/DAGRwkonF2U/BxfrNpGMXpUIbae_88OZFg/edit?utm_content=DAGRwkonF2U&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
       category: "Graphic Design"
     },
@@ -55,7 +55,7 @@ export const projects = [
       description: "A web-based system designed to efficiently monitor on-the-job training activities using QR code technology for real-time attendance tracking, task validation, and trainee progress management.",
       technologies: ["PHP", "HTMl", "CSS"],
       image: "/project-mrbrew.png",
-      githubLink: "*",
+      githubLink: "404 Not Found",
       liveLink: "*",
       category: "Web Application"
     },
